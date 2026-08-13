ca008b909fcf91176f6a7d288ebdf5d694140105
GoogleyEyes <froginapond10@gmail.com>
2026-08-13 12:57:33 +1000

    Better node system for ingredients

--- Alchamess/Assets/Scripts/Ingredient.gd
+++ Alchamess/Assets/Scripts/Ingredient.gd
@@ -1,14 +1,28 @@
 extends CharacterBody2D
 
 var is_grabbed : bool = false
-var starting_pos = position
 
 func _process(delta):
 	if is_grabbed:
 		# Checks if the left mouse button has been released
 		if not Input.is_mouse_button_pressed(MOUSE_BUTTON_LEFT):
 			is_grabbed = false
-			position = starting_pos
+			
+			# Returning the ingredients to their starting positions
+			match name:
+				"EyeOfNewt":
+					global_position = Vector2(157, 171)
+				"Wormwood":
+					global_position = Vector2(994, 170)
+				"MagicPebble":
+					global_position = Vector2(157, 299)
+				"PhoenixFeather":
+					global_position = Vector2(994, 298)
+				"OilOfVitriol":
+					global_position = Vector2(157, 427)
+				"Stardust":
+					global_position = Vector2(994, 426)
+			
 			return
 		
 		# Moves the ingredient with the mouse if grabbed
@@ -18,10 +32,11 @@ func _process(delta):
 		
 func _input_event(viewport, event, shape_idx) -> void:
 	# Checks if the ingredient has been grabbed
-	if event is InputEventMouseButton and event.button_index == MOUSE_BUTTON_LEFT:
-		if event.pressed:
+	if event is InputEventMouseButton:
+		if event.button_index == MOUSE_BUTTON_LEFT and event.pressed:
 			is_grabbed = true
-		else:
-			is_grabbed = false
-			position = starting_pos
-			
+
+# Detecting if the ingredient is placed in the cauldron
+func _on_detection_area_body_entered(body: Node2D) -> void:
+	if is_grabbed:
+		print(name, " has been placed in the pot")
