5b44628cc73a3f16a4e69f8922e6b401e2bcf04e
GoogleyEyes <froginapond10@gmail.com>
2026-08-20 12:53:21 +1000

    Potion of Baldness combo

--- Alchamess/Assets/Scripts/Cauldron.gd
+++ Alchamess/Assets/Scripts/Cauldron.gd
@@ -3,6 +3,11 @@ extends CharacterBody2D
 # A dictionary to store all the recieved ingredients
 var CauldronIngredients: Dictionary = {}
 
+# A dictionary to store all potion combinations
+var PotionRecipes: Dictionary = {
+	["EyeOfNewt", "OilOfVitriol", "Wormwood"]: "PotionOfBaldness"
+}
+
 # Potion bottles
 @onready var Potion = $"../CraftedPotion"
 
@@ -17,13 +22,9 @@ func _ready():
 
 
 func _add_ingredient_to_cauldron(name):
-	if CauldronIngredients.size() >= 3:
+	if CauldronIngredients.size() > 3:
 		# If cauldron already has 3 ingredients, don't add another
 		print("Cauldron full")
-		
-		# Spawning potion, resetting ingredients
-		Potion.visible = true
-		CauldronIngredients = {}
 		
 		return
 	
@@ -34,6 +35,33 @@ func _add_ingredient_to_cauldron(name):
 	
 	# Add ingredient to the pot
 	CauldronIngredients[name] = 1
-	
 	print(name, " has been placed in the pot")
+	
+	if CauldronIngredients.size() == 3:
+		# Create the potion based on ingredient combination
+		create_potion()
+		
+		# Spawning potion sprite, resetting ingredients
+		Potion.visible = true
+		CauldronIngredients = {}
+	
 	print("CURRENT CAULDRON INGREDIENTS: ", CauldronIngredients)
+	
+	
+func create_potion():
+		# Getting the current cauldron ingredients and sorting them
+		var Ingredients = CauldronIngredients.keys()
+		
+		# Converting the node StringNames to Strings
+		for i in range(Ingredients.size()):
+			Ingredients[i] = str(Ingredients[i])
+		
+		# Sorting them alphabetically
+		Ingredients.sort()
+		
+		if PotionRecipes.has(Ingredients):
+			var Potion = PotionRecipes[Ingredients]
+			print("Created: ", Potion)
+		else:
+			print(Ingredients)
+			print("Invalid combination")
