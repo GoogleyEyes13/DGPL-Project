c8beb7f45311746a6ca5d525ebf1906a29e21806
Bones-Malone <67958852+Bones-Malone@users.noreply.github.com>
2026-09-02 13:31:00 +1000

    added global variables and tweaks to dialogue

--- Alchamess/Assets/Scripts/Customer.gd
+++ Alchamess/Assets/Scripts/Customer.gd
@@ -71,8 +71,10 @@ func bob_in() -> void:
 func on_arrival() -> void:
 	global_position.y = centre_pos.y
 	print("Customer is at the counter! Waiting for interaction...") #test
-	var resource = load("res://dialogue/testDialogue.dialogue")
-	DialogueManager.show_dialogue_balloon(resource)
+	match CustomerName:
+		"Caseoh":
+			var resource = load("res://dialogue/testDialogue.dialogue")
+			DialogueManager.show_dialogue_balloon(resource)
 	cust_is_ready = true
 
 func receive_potion(potion_type: String) -> void:
