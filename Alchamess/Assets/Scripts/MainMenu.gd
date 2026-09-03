6460a24ff6117a39dba882390a53b2df2aac1dd8
terradaks <169633654+terradaks@users.noreply.github.com>
2026-09-03 19:46:10 +1000

    Added the functionality for options menu :)

--- Alchamess/Assets/Scripts/MainMenu.gd
+++ Alchamess/Assets/Scripts/MainMenu.gd
@@ -1,12 +1,16 @@
-extends Node2D
+extends Control
+
+@onready var settings_button: Button = $SettingsButton
+@onready var settings_menu: Control = $SettingsMenu
+
+func _ready() -> void:
+	settings_button.pressed.connect(_on_settings_pressed)
 
 func _on_play_button_pressed() -> void:
 	get_tree().change_scene_to_file("res://Scenes/Game.tscn")
 
-
-func _on_option_button_pressed() -> void:
-	pass # Replace with function body.
-
+func _on_settings_pressed() -> void:
+	settings_menu.show()
 
 func _on_quit_button_pressed() -> void:
 	get_tree().quit()
